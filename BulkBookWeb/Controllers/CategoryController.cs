f281126b3c0bc898b68e475852454f12ec2d625f
Omri <omria60@gmail.com>
2022-04-03 21:37:51 +0300

    Add project files.

---
+++ BulkBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using BulkBookWeb.Data;
+using BulkBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+    }
+}
---
+++ BulkBookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using BulkBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkBookWeb.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
